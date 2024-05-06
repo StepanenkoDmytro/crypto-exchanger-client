8b7f67dd56aa78eb09b93ec8857617186d64cc7a
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-05-06 15:20:45 +0300

    connect with db; basic logic for convert

--- src/components/SimpleExchanger.tsx
+++ src/components/SimpleExchanger.tsx
@@ -1,72 +1,130 @@
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
+import { useState, useEffect, ChangeEvent } from 'react';
 
-const currencies = [
-    {
-      value: 'USD',
-      label: '$',
-    },
-    {
-      value: 'EUR',
-      label: '€',
-    },
-    {
-      value: 'BTC',
-      label: '฿',
-    },
-    {
-      value: 'JPY',
-      label: '¥',
-    },
-  ];
+interface IConvert {
+    id: string;
+    name: string;
+    price: number | null;
+    symbol: string;
+    amount: number | string;
+}
 
 const SimpleExchanger: React.FC = () => { 
+    const currencyToConvertMock: IConvert = {
+        id: "bitcoin",
+        name: "Bitcoin",
+        price: null,
+        symbol: "BTC",
+        amount: 0.1
+    }
+
+    const convertedCurrencyMock: IConvert = {
+        id: "ethereum",
+        name: "Ethereum",
+        price: null,
+        symbol: "ETH",
+        amount: 0
+    }
+    const [isLoading, setIsLoading] = useState(true);
+    const [currencyToConvert, setCurrencyToConvert] = useState<IConvert>(currencyToConvertMock);
+    const [convertedCurrency, setConvertedCurrency] = useState<IConvert>(convertedCurrencyMock);
+    const [amount, setAmount] = useState<number | string>(currencyToConvert.amount);
+    const [convertedAmount, setConvertedAmount] = useState<number | null>(0);
+
+    useEffect(() => {
+        convertCurrency();
+        if (!isLoading) {
+            return;
+        }
+        if (currencyToConvert && convertedCurrency) {
+            fetch(`http://localhost:8080/api/v1/crypto/list?tickers=${currencyToConvert.id}&tickers=${convertedCurrency.id}`)
+                .then(response => response.json())
+                .then((coinsInfo: any) => {
+                    const currencyToConvertInfo = coinsInfo.find((coin: any) => coin.id === currencyToConvert.id);
+                    const convertedCurrencyInfo = coinsInfo.find((coin: any) => coin.id === convertedCurrency.id);
+
+                    if (currencyToConvertInfo) {
+                        setCurrencyToConvert(prevState => ({
+                            ...prevState,
+                            price: currencyToConvertInfo.price
+                        }));
+                    }
+
+                    if (convertedCurrencyInfo) {
+                        setConvertedCurrency(prevState => ({
+                            ...prevState,
+                            price: convertedCurrencyInfo.price
+                        }));
+                    }
+                    
+                    setIsLoading(false);
+                })
+        }
+
+    }, [currencyToConvert, convertedCurrency, amount, isLoading]);
+
+    const convertCurrency = () => {
+        const parseAmount = typeof amount === 'string' ? parseFloat(amount) : amount;
+        if (parseAmount && currencyToConvert.price && convertedCurrency.price) {
+            const result = (parseAmount * currencyToConvert.price / convertedCurrency.price).toFixed(6);
+            setConvertedAmount(parseFloat(result));
+        }
+    };
+
+    const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+        
+        setAmount(inputValue);
+    };  
+    
+
     return (
         <Card sx={{ maxWidth: 345 }}>
             <div>
                 <TextField
-                    id="standard-error"
-                    label="Error"
-                    defaultValue="Hello World"
+                    id="standard-basic"
+                    label="Amount to send"
+                    value={amount}
+                    onChange={handleAmountChange}
                     variant="standard"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
                 />
                 <TextField
                     id="standard-select-currency"
                     select
                     label="Select"
-                    defaultValue="EUR"
+                    value={currencyToConvert.symbol}
                     helperText="Please select your currency"
                     variant="standard"
                 >
-                    {currencies.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                        </MenuItem>
-                    ))}
+                    <MenuItem key={currencyToConvert.id} value={currencyToConvert.symbol}>
+                        {currencyToConvert.name}
+                    </MenuItem>
                 </TextField>
             </div>
             <div>
                 <TextField
                     id="standard-error"
-                    label="Error"
-                    defaultValue="Hello World"
+                    label="Amount to receive"
+                    value={convertedAmount}
                     variant="standard"
+                    disabled
                 />
                 <TextField
                     id="standard-select-currency"
                     select
                     label="Select"
-                    defaultValue="EUR"
+                    defaultValue={convertedCurrency.symbol}
                     helperText="Please select your currency"
                     variant="standard"
                 >
-                    {currencies.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                        </MenuItem>
-                    ))}
+                    <MenuItem key={convertedCurrency.id} value={convertedCurrency.symbol}>
+                        {convertedCurrency.name}
+                    </MenuItem>
                 </TextField>
             </div>
         </Card>
