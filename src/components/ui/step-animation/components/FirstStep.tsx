bcb4243d2675319d2cf95f652614fc6f988c4b0b
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-06-20 00:09:55 +0300

    try change line img for step-animation

--- src/components/ui/step-animation/components/FirstStep.tsx
+++ src/components/ui/step-animation/components/FirstStep.tsx
@@ -15,7 +15,12 @@ const FirstStep: React.FC = () => {
                     <p className='step-item__content-title'>{t('exchanger.explaneHeader.stepOne.title')}</p>
                     <p className='step-item__content-desc'>{t('exchanger.explaneHeader.stepOne.desc')}</p>
                 </div>
-                <div className='step-item__line'><img src={connectLinePNG} alt="step-line-png"/></div>
+                <div className='step-item__line-box'>
+                    
+                    <div className='step-item__line'></div>
+                    <div className='step-item__circle'><span></span></div>
+                    {/* <img src={connectLinePNG} alt="step-line-png"/> */}
+                </div>
             </div>
 
             <div className='step-item'>
@@ -25,7 +30,13 @@ const FirstStep: React.FC = () => {
                     <p className='step-item__content-title'>{t('exchanger.explaneHeader.stepTwo.title')}</p>
                     <p className='step-item__content-desc'>{t('exchanger.explaneHeader.stepTwo.desc')}</p>
                 </div>
-                <div className='step-item__line'><img src={connectLinePNG} alt="step-line-png"/></div>
+                <div className='step-item__line-box'>
+                    {/* <span className='step-item_circle step-item_circle-left'></span> */}
+                    <div className='step-item__circle step-item_circle-left'><span></span></div>
+                    <div className='step-item__line'></div>
+                    <div className='step-item__circle'><span></span></div>
+                    {/* <img src={connectLinePNG} alt="step-line-png"/> */}
+                </div>
             </div>
         </div>
     );
--- src/components/ui/step-animation/components/SecondStep.tsx
+++ src/components/ui/step-animation/components/SecondStep.tsx
@@ -9,14 +9,24 @@ const SecondStep: React.FC = () => {
     return (
         <div className='step-box'>
             <div className='step-item'>
-            <div className='step-item__line prev-line'><img src={connectLinePNG} alt="step-line-png"/></div>
+            <div className='step-item__line-box prev-line'>
+                
+                <div className='step-item__line'></div>
+                
+                {/* <img src={connectLinePNG} alt="step-line-png"/> */}
+            </div>
                 <div className='step-item__img'><img src={walletPNG} alt="step-png"/></div>
                 <div className='step-item__content'>
                     <p className='step-item__content-number'>{t('exchanger.explaneHeader.stepTwo.step')}</p>
                     <p className='step-item__content-title'>{t('exchanger.explaneHeader.stepTwo.title')}</p>
                     <p className='step-item__content-desc'>{t('exchanger.explaneHeader.stepTwo.desc')}</p>
                 </div>
-                <div className='step-item__line'><img src={connectLinePNG} alt="step-line-png"/></div>
+                <div className='step-item__line-box'>
+                    <div className='step-item__circle step-item_circle-left'><span></span></div>
+                    <div className='step-item__line'></div>
+                    <div className='step-item__circle'><span></span></div>
+                    {/* <img src={connectLinePNG} alt="step-line-png"/> */}
+                </div>
             </div>
 
             <div className='step-item'>
@@ -26,7 +36,12 @@ const SecondStep: React.FC = () => {
                     <p className='step-item__content-title'>{t('exchanger.explaneHeader.stepThree.title')}</p>
                     <p className='step-item__content-desc'>{t('exchanger.explaneHeader.stepThree.desc')}</p>
                 </div>
-                <div className='step-item__line'><img src={connectLinePNG} alt="step-line-png"/></div>
+                <div className='step-item__line-box'>
+                    <div className='step-item__circle step-item_circle-left'><span></span></div>
+                    <div className='step-item__line'></div>
+                    <div className='step-item__circle'><span></span></div>
+                    {/* <img src={connectLinePNG} alt="step-line-png"/> */}
+                </div>
             </div>
         </div>
     );
