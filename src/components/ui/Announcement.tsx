e85df2ff745f790b39fbaa0055f7e3d91c79360a
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-06-24 15:24:12 +0300

    Announcement with animation

--- src/components/ui/Announcement.tsx
+++ src/components/ui/Announcement.tsx
@@ -12,15 +12,14 @@ const Announcement: React.FC = () => {
             <div className='announcement__box'>
                 <div className='announcement__contect'>
                     <img src={blumIcon}/>
-                    <p><span>Blum</span> - это криптовалютное торговое приложение, биржа в мессенджере Telegram. На гибридной CeDeFi модели.</p>
                 </div>
             
                 <div className='announcement__desc'>
                     <p className='announcement__desc-title'>New Coins Coming Soon to Rocky Finance!</p>
                     <p>Stay one step ahead with RockyFinance! New coins are coming soon to our platform: </p>
+                    <p><span>Blum</span> - это криптовалютное торговое приложение, биржа в мессенджере Telegram. На гибридной CeDeFi модели.</p>
                 </div>
             </div>
-            {/* <p className='announcement__end'>Get ready for a new level of investing!</p> */}
         </div>
         
     </section>
